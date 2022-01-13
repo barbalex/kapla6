b3880985225c3423f6c5011b840d426d97d8712b
Alexander Gabriel <alex.barbalex@gmail.com>
2022-01-13 14:26:12 +0100

    get username

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,8 +5,14 @@
 
 use tauri_plugin_sql::TauriSql;
 
+#[tauri::command]
+fn get_username() -> String {
+    whoami::username()
+}
+
 fn main() {
   tauri::Builder::default()
+  .invoke_handler(tauri::generate_handler![get_username])
   .plugin(TauriSql::default())
     .run(tauri::generate_context!())
      .expect("error while running tauri application");
--- src/src/fetchUsername.js
+++ src/src/fetchUsername.js
@@ -1,9 +1,9 @@
-//import { username as getUsername } from 'username'
+import { invoke } from '@tauri-apps/api/tauri'
 
 const fetchUsername = async (store) => {
   const { setUsername } = store.app
-  //const username = await getUsername()
-  const username = 'TODO: implement with tauri'
+  const username = await invoke('get_username')
+  console.log('username:', username)
 
   if (username) {
     setUsername(username)
