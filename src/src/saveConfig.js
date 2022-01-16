8e6f44c6b9e0ad3245cf5c1673c5c73853edc5e2
Alexander Gabriel <alex.barbalex@gmail.com>
2022-01-16 11:27:54 +0100

    clean

--- src/src/saveConfig.js
+++ src/src/saveConfig.js
@@ -2,7 +2,7 @@ import { fs, path } from '@tauri-apps/api'
 
 const saveConfig = async (data) => {
   const userPath = await path.appDir() // C:\Users\alexa\AppData\Roaming\Kapla\
-  // tauri: https://tauri.studio/en/docs/api/js/modules/fs#writefile
+
   return fs.writeFile({
     contents: JSON.stringify(data, null, 2),
     path: `${userPath}kaplaConfig.json`,
