911cbb5318f87c3659262ebebe303c74203e07a3
Alexander Gabriel <alex.barbalex@gmail.com>
2022-01-24 14:01:31 +0100

    remove mini-css-extract-plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -28,28 +28,28 @@ use sqlx::sqlite::SqlitePoolOptions;
 use async_std::task;
 
 //#[async_std::main]
-#[tauri::command]
-async fn fts_search(dbpath: &str, searchtext: &str) -> Result<Vec<i64>, sqlx::Error> {
-  let connection_string = format!("sqlite://{}", dbpath);
-  let pool = SqlitePoolOptions::new()
-      .max_connections(5)
-      .connect(&connection_string).await?;
+// #[tauri::command]
+// async fn fts_search(dbpath: &str, searchtext: &str) -> Result<Vec<i64>, sqlx::Error> {
+//   let connection_string = format!("sqlite://{}", dbpath);
+//   let pool = SqlitePoolOptions::new()
+//       .max_connections(5)
+//       .connect(&connection_string).await?;
 
-  #[derive(sqlx::FromRow)]
-  struct IdGeschaeft { idGeschaeft: i64 }
-  let rows = sqlx::query_as::<_, IdGeschaeft>("select idGeschaeft from fts where value match '\"?\"*'")
-      .bind(searchtext)
-      .fetch_one(&pool);
+//   #[derive(sqlx::FromRow)]
+//   struct IdGeschaeft { idGeschaeft: i64 }
+//   let rows = sqlx::query_as::<_, IdGeschaeft>("select idGeschaeft from fts where value match '\"?\"*'")
+//       .bind(searchtext)
+//       .fetch_one(&pool);
 
-  while let Some(row) = rows.try_next().await? {
-    // map the row into a user-defined domain type
-    let idGeschaeft: i64 = row.try_get("idGeschaeft")?;
-  }
-}
+//   while let Some(row) = rows.try_next().await? {
+//     // map the row into a user-defined domain type
+//     let idGeschaeft: i64 = row.try_get("idGeschaeft")?;
+//   }
+// }
 
 fn main() {
   tauri::Builder::default()
-  .invoke_handler(tauri::generate_handler![get_username, exists_file,fts_search])
+  .invoke_handler(tauri::generate_handler![get_username, exists_file])
   .plugin(TauriSql::default())
     .run(tauri::generate_context!())
      .expect("error while running tauri application");
