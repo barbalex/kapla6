57050e3570c57e5cf576f4fb3d6220080373ab31
Alexander Gabriel <alex.barbalex@gmail.com>
2022-07-01 21:17:31 +0200

    reset data dir

--- src/src/getConfig.js
+++ src/src/getConfig.js
@@ -1,7 +1,7 @@
 import { fs, path } from '@tauri-apps/api'
 
 const getConfig = async () => {
-  const userPath = await path.dataDir() // C:\Users\alexa\AppData\Roaming\Kapla\
+  const userPath = await path.appDir() // C:\Users\alexa\AppData\Roaming\Kapla\
   console.log('getConfig, userPath: ', userPath)
   const dataFilePath = `${userPath}kaplaConfig.json`
   let configFile
