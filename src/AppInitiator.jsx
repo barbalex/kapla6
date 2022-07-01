8cb23a4c4efeff02f4ef13c0264b9b419f88d105
Alexander Gabriel <alex.barbalex@gmail.com>
2022-07-01 15:02:42 +0200

    prepare for vite

---
+++ src/AppInitiator.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import { createRoot } from 'react-dom/client'
+import { registerLocale, setDefaultLocale } from 'react-datepicker'
+import { de } from 'date-fns/locale'
+
+import App from './components/App'
+import { Provider as StoreProvider } from './storeContext'
+import createStore from './store'
+import './styles.css'
+import getDb from './src/getDb'
+import initiate from './src/initiate'
+
+registerLocale('de', de)
+setDefaultLocale('de')
+
+const AppInitator = () => {
+  const [store, setStore] = useState()
+
+  useEffect(() => {
+    const store = createStore().create()
+    setStore(store)
+    getDb(store)
+      .then((db) => {
+        store.app.setDb(db)
+        initiate(store)
+      })
+      .catch((error) => {
+        console.log('error getting db:', error)
+        store.addErrorMessage(error.message)
+      })
+  }, [])
+
+  if (!store) return null
+
+  return (
+    <StoreProvider value={store}>
+      <App />
+    </StoreProvider>
+  )
+}
+
+export default AppInitator
--- src/index.js
+++ src/index.js
@@ -1,39 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { registerLocale, setDefaultLocale } from 'react-datepicker'
-import { de } from 'date-fns/locale'
 
-import App from './components/App'
-import { Provider as StoreProvider } from './storeContext'
-import createStore from './store'
-import './styles.css'
-import getDb from './src/getDb'
-import initiate from './src/initiate'
+import AppInitator from './AppInitiator'
 
-const run = async () => {
-  const store = await createStore().create()
+const container = document.getElementById('root')
+const root = createRoot(container)
 
-  let db
-  try {
-    db = await getDb(store)
-  } catch (error) {
-    console.log('error getting db:', error)
-    store.addErrorMessage(error.message)
-  }
-  store.app.setDb(db)
-  initiate(store)
-
-  registerLocale('de', de)
-  setDefaultLocale('de')
-
-  const container = document.getElementById('root')
-  const root = createRoot(container) // create
-
-  root.render(
-    <StoreProvider value={store}>
-      <App />
-    </StoreProvider>,
-  )
-}
-
-run()
+root.render(<AppInitator />)
