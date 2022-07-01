4ccde73f6832a5ad2fb74e801adb585f4a02c87f
Alexander Gabriel <alex.barbalex@gmail.com>
2022-07-01 21:16:09 +0200

    optimize

--- src/src/saveConfig.js
+++ src/src/saveConfig.js
@@ -1,12 +1,23 @@
 import { fs, path } from '@tauri-apps/api'
 
 const saveConfig = async (data) => {
+  console.log('saveConfig',{data})
   const userPath = await path.appDir() // C:\Users\alexa\AppData\Roaming\Kapla\
+  const contents = JSON.stringify(data, null, 2)
+  const _path = `${userPath}kaplaConfig.json`
 
-  return fs.writeFile({
-    contents: JSON.stringify(data, null, 2),
-    path: `${userPath}kaplaConfig.json`,
-  })
+  console.log('saveConfig',{userPath,path:_path,contents})
+
+  try {
+    await fs.writeTextFile({
+      contents,
+      path:_path,
+    })
+  } catch (error) {
+    console.log('saveConfig, error writing config:',error)
+  }
+
+  return
 }
 
 export default saveConfig
